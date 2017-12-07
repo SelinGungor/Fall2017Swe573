3757dcf68ab818d6750e87351633831dea8b35d1
selin.gungor <selingungor01@gmail.com>
2017-12-08 00:02:56 +0100

    Unit test is added

--- swe573/personal/tests.py
+++ swe573/personal/tests.py
@@ -7,3 +7,7 @@ class TestSentiment:
     def test_sentiment(self):
         response = se.sentiment("happy")
         assert response is not None
+
+    def test_sentiment_2(self):
+        response = se.sentiment("sad")
+        assert response is not None
