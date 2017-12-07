2c008a578a1db436d0ee963f785bc234acfa19a8
selin.gungor <selingungor01@gmail.com>
2017-12-07 22:20:51 +0100

    django setup

--- myapp/swe573/swe573/settings.py
+++ myapp/swe573/swe573/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import django
 import os
 
+
+django.setup()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ALGO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
